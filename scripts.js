21521247b9bc5b786d6f89133c17ba3e4aa8a233
Jarol20cb <u20201b559@upc.edu.pe>
2024-07-24 22:20:46 -0500

    se mejoro las animaciones y el scrolet

--- scripts.js
+++ scripts.js
@@ -1,21 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
-    const mainNav = document.querySelector('.main-nav ul');
-  
-    mobileMenuIcon.addEventListener('click', function() {
-      mainNav.classList.toggle('show');
-    });
-  
-    document.addEventListener('click', function(event) {
-      if (!mobileMenuIcon.contains(event.target) && !mainNav.contains(event.target)) {
-        mainNav.classList.remove('show');
-      }
-    });
-  
-    mainNav.querySelectorAll('a').forEach(link => {
-      link.addEventListener('click', () => {
-        mainNav.classList.remove('show');
-      });
+  const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+  const mainNav = document.querySelector('.main-nav ul');
+  const header = document.querySelector('header');
+
+  mobileMenuIcon.addEventListener('click', function() {
+    mainNav.classList.toggle('show');
+  });
+
+  document.addEventListener('click', function(event) {
+    if (!mobileMenuIcon.contains(event.target) && !mainNav.contains(event.target)) {
+      mainNav.classList.remove('show');
+    }
+  });
+
+  mainNav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      mainNav.classList.remove('show');
     });
   });
-  
+
+  window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+          header.classList.add('scrolled');
+      } else {
+          header.classList.remove('scrolled');
+      }
+  });
+
+  // Smooth scroll for menu links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+});
